8627334822aef5a9132950494ba9fce98f26bdcd
Mateus-Fonseca847 <matfonseca2016@gmail.com>
2026-03-20 12:40:52 -0300

    Header Site

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(os.path.dirname(__file__), "../frontend/templates"),
+    static_folder=os.path.join(os.path.dirname(__file__), "../frontend/static")
+)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
